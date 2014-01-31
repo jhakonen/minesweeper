6f8b4c97e61c2db5f671b88f40f0821abf954ef3
Janne Hakonen <janne.hakonen@tieto.com>
2014-01-31 23:00:12 +0200

    Use a class for tests.
    Signed-off-by: Janne Hakonen <janne.hakonen@tieto.com>

--- usecases/test/test_createnewgame.py
+++ usecases/test/test_createnewgame.py
@@ -2,18 +2,15 @@ from minesweeper.usecases.createnewgame import CreateNewGameUseCase
 from nose.tools import with_setup
 import mock
 
-def setup():
-	global sink
-	sink = mock.Mock()
-	global uc
-	uc = CreateNewGameUseCase(sink)
+class TestCreateNewGame(object):
+	def setUp(self):
+		self.sink = mock.Mock()
+		self.uc = CreateNewGameUseCase(self.sink)
 
-@with_setup(setup)
-def test_create_new_game_calls_game_created():
-	uc.create_new_game()
-	assert sink.game_created.called
+	def test_create_new_game_calls_game_created(self):
+		self.uc.create_new_game()
+		assert self.sink.game_created.called
 
-@with_setup(setup)
-def test_create_new_game_calls_board_size_changed():
-	uc.create_new_game()
-	sink.board_size_changed.assert_called_once_with(9, 9)
+	def test_create_new_game_calls_board_size_changed(self):
+		self.uc.create_new_game()
+		self.sink.board_size_changed.assert_called_once_with(9, 9)
